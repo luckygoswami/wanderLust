749266e1ffa7488aa8fedbc6c13e32a6f9164326
Lucky Goswami <lgoswami204@gmail.com>
2023-10-06 18:34:56 +0530

    build: added prerequisites

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.get("/", (req, res) => {
+    res.send("root page");
+});
+
+app.listen(8080, () => {
+    console.log("listening to port 8080");
+});
