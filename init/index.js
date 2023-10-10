5bc4806332b96560c6af83cc414edd4364587ed3
Lucky Goswami <lgoswami204@gmail.com>
2023-10-10 12:49:43 +0530

    test: initialized some data

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        console.log(`there's an error`);
+    });
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data initialized");
+};
+
+initDB();
