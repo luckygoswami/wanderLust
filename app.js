f64243dbb28c4587791a96732642f500f93938dd
Lucky Goswami <lgoswami204@gmail.com>
2023-10-10 14:59:07 +0530

    feat: created an index route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,17 +25,23 @@ app.get("/", (req, res) => {
     res.send("root page");
 });
 
-app.get("/testlisting", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "my new villa",
-        description: "by the beach",
-        price: 500,
-        location: "Goa",
-        country: "India",
-    });
-    await sampleListing.save();
-    console.log("sample saved");
-    res.send("successful");
+// app.get("/testlisting", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "my new villa",
+//         description: "by the beach",
+//         price: 500,
+//         location: "Goa",
+//         country: "India",
+//     });
+//     await sampleListing.save();
+//     console.log("sample saved");
+//     res.send("successful");
+// });
+
+// Index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
 });
 
 app.listen(8080, () => {
