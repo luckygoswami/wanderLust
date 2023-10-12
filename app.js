5e73a337660190bdaf72923565cb82d31310ea95
Lucky Goswami <lgoswami204@gmail.com>
2023-10-12 15:59:27 +0530

    feat: Edit route created

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -62,6 +64,13 @@ app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
+});
+
+//Edit route
+app.put("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
 });
 
 app.listen(8080, () => {
