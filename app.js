c5fd77a55be84be263a56112cff26f4923e578e5
Lucky Goswami <lgoswami204@gmail.com>
2023-10-20 21:54:40 +0530

    perf: refactored the code

--- app.js
+++ app.js
@@ -130,7 +130,6 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "oops! something went wrong." } = err;
     res.status(statusCode).render("error.ejs", { message });
-    // res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
