d88b93dd1e1a25941f20a5cf8f8ee1135e59a783
Lucky Goswami <lgoswami204@gmail.com>
2023-10-26 12:15:32 +0530

    feat: added server side validation for reviews

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 app.set("view engine", "ejs");
@@ -37,6 +37,16 @@ app.get("/", (req, res) => {
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, error);
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, error);
@@ -112,14 +122,18 @@ app.delete(
 );
 
 //Review Route
-app.post("/listings/:id/reviews", async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
+app.post(
+    "/listings/:id/reviews",
+    validateReview,
+    wrapAsync(async (req, res) => {
+        let listing = await Listing.findById(req.params.id);
+        let newReview = new Review(req.body.review);
 
-    Listing.reviews.push(newReview);
-    await newReview.save();
-    await Listing.save();
-});
+        Listing.reviews.push(newReview);
+        await newReview.save();
+        await Listing.save();
+    })
+);
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
