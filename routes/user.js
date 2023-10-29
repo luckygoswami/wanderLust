65097101ce5b8b498be732c6b70e1490e3213b17
Lucky Goswami <lgoswami204@gmail.com>
2023-10-29 20:49:32 +0530

    feat: post request created for signup form

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,26 @@
 const express = require("express");
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
+
+router.post(
+    "/signup",
+    wrapAsync(async (req, res) => {
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ username, email });
+            await User.register(newUser, password);
+            req.flash("success", "Welcome to WanderLust");
+            res.redirect("/listings");
+        } catch (e) {
+            req.flash("error", e.message);
+            res.redirect("/signup");
+        }
+    })
+);
 
 module.exports = router;
