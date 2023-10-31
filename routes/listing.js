a7a8a7cbddcd5632c53d06164eb91544f7606142
Lucky Goswami <lgoswami204@gmail.com>
2023-10-31 19:48:21 +0530

    refactor: some prettier formatting added

--- routes/listing.js
+++ routes/listing.js
@@ -5,15 +5,32 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
 // Index route
-router.route("/").get(wrapAsync(listingController.index)).post(validateListing, wrapAsync(listingController.createListing));
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(validateListing, wrapAsync(listingController.createListing));
 
 // New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Show route
-router.route("/:id").get(wrapAsync(listingController.showListing)).put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing)).delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        validateListing,
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 //Edit route
-router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.editListing));
+router.get(
+    "/:id/edit",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.editListing)
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,10 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js");
 
-router.route("/signup").get(userController.renderSignupForm).post(wrapAsync(userController.signup));
+router
+    .route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
 
 router
     .route("/login")
