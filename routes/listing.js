20c44710f67cbb319f4ecb7f1380a1c016fdc62e
Lucky Goswami <lgoswami204@gmail.com>
2023-10-31 14:30:56 +0530

    refactor: added all middlewares in one file

--- routes/listing.js
+++ routes/listing.js
@@ -2,19 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
-const { isLoggedIn, isOwner } = require("../middleware.js");
-
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, error);
-    } else {
-        next();
-    }
-};
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 // Index route
 router.get(
