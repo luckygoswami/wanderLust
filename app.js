fd958c0dc21d9fa5205a0cf82cfcb60740e0e0a5
Lucky Goswami <lgoswami204@gmail.com>
2023-11-02 19:34:48 +0530

    feat: project ready for deployment

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -41,8 +42,21 @@ async function main() {
     await mongoose.connect(mongo_url);
 }
 
+const store = MongoStore.create({
+    mongoUrl: mongo_url,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("Error in mongo session store", err);
+});
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
